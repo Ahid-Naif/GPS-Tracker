c7dcffd24b916fd1a90601c8276615d57bda5ba7
unknown <ahedqaood19@gmail.com>
2022-03-03 20:16:48 +0300

    add database table

--- database/migrations/2022_03_03_200654_gps_values.php
+++ database/migrations/2022_03_03_200654_gps_values.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateOrdersTable extends Migration
+class GpsValues extends Migration
 {
     /**
      * Run the migrations.
@@ -13,17 +13,11 @@ class CreateOrdersTable extends Migration
      */
     public function up()
     {
-        Schema::create('orders', function (Blueprint $table) {
+        Schema::create('gps_values', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('owner_id');
-            $table->float('total_price');
-            $table->string('code');
+            $table->string('name');
+            $table->string('value');
             $table->timestamps();
-
-            $table->foreign('owner_id')
-                ->references('id')
-                ->on('users')
-                ->onDelete('cascade');
         });
     }
 
@@ -34,6 +28,6 @@ class CreateOrdersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('orders');
+        Schema::dropIfExists('gps_values');
     }
 }
