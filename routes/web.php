eacc980e47200ffdd004543d566cf0c2a3c34e8f
unknown <ahedqaood19@gmail.com>
2022-03-03 20:16:56 +0300

    update

--- routes/web.php
+++ routes/web.php
@@ -15,5 +15,52 @@ use Illuminate\Http\Request;
 */
 
 Route::get('/', function(){
-   return view('gps_tracking');
+   $latitude = DB::table('gps_values')->where('name', 'latitude')->get()[0]->value;
+   $longitude = DB::table('gps_values')->where('name', 'longitude')->get()[0]->value;
+   $speed = DB::table('gps_values')->where('name', 'speed')->get()[0]->value;
+   $direction = DB::table('gps_values')->where('name', 'direction')->get()[0]->value;
+   $satellites = DB::table('gps_values')->where('name', 'satellites')->get()[0]->value;
+
+   return view('gps_tracking', compact(
+      'latitude',
+      'longitude',
+      'speed',
+      'direction',
+      'satellites',
+   ));
+});
+
+Route::get('/sendValues', function () {
+
+   if(isset($_GET['latitude']))
+   {
+      $latitude = $_GET['latitude'];
+      DB::table('gps_values')->where('name', 'latitude')->update(['value' => $latitude]);
+   }
+
+   if(isset($_GET['longitude']))
+   {
+      $longitude = $_GET['longitude'];
+      DB::table('gps_values')->where('name', 'longitude')->update(['value' => $longitude]);
+   }
+
+   if(isset($_GET['speed']))
+   {
+      $speed = $_GET['speed'];
+      DB::table('gps_values')->where('name', 'speed')->update(['value' => $speed]);
+   }
+
+   if(isset($_GET['direction']))
+   {
+      $direction = $_GET['direction'];
+      DB::table('gps_values')->where('name', 'direction')->update(['value' => $direction]);
+   }
+
+   if(isset($_GET['satellites']))
+   {
+      $satellites = $_GET['satellites'];
+      DB::table('gps_values')->where('name', 'satellites')->update(['value' => $satellites]);
+   }
+
+   return "was added succefully";
 });
